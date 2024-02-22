a0bf85769de871e3756d5e78c6e41394cda15a6f
JimmySilentG <jimmygnesda@gmail.com>
2024-02-22 18:40:35 -0500

    no ACM0 error...

--- src/SerialRxTest.py
+++ src/SerialRxTest.py
@@ -4,15 +4,21 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0',115200,timeout = None) #open serial port assigned to teensy, this can change between ACMO and ACM1, set baudrate to 115200 even though teensy ignores it. timeout set to none so the port waits forever until the requested number of bytes are recieved
 
-DutyCycle = 98.2316 #message saved to local attribute
+DutyCycle = 50.00001 #message saved to local attribute
 DutyCycleBytes = struct.pack('f', DutyCycle) #package python float into 4 byte transmission to be send off(check struct documentation for byte order if necessary)
+for i in range(0,19):
+    ser.write(b'\xa0')
+    print(i)
+
 ser.write(b'\xaa') #first write two 170 bytes to serial to tell teensy next 4 bytes are pid command
 ser.write(b'\xaa') #this technique is susiptible to dropped and partial packets but at 250hz we should be fine with 1 or 2 bad packets
 ser.write(DutyCycleBytes) #write floating point duty cycle from PID calculation to serial for teensy to see
 
-time.sleep(0.1)
+time.sleep(0.25)
 
-print(len(DutyCycleBytes))
-response_bytes = ser.read(len(DutyCycleBytes))
-response_value = struct.unpack('f', response_bytes)[0]
-print(response_value)
+#print(len(DutyCycleBytes))
+while True:
+    response_bytes = ser.read(len(DutyCycleBytes))
+    response_value = struct.unpack('f', response_bytes)[0]
+    print(response_value)
+    time.sleep(0.01)
--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,12 @@ void setup() {
 
 unsigned long axis1 = 0;
 //unsigned long axis2 = 0;
-unsigned long counter = 0;
+unsigned long counter = 0; //used for programmatically incrementing encoder while not hooked up
+byte b1[1]; //temporary buffers used only to step through the incoming message and find the start bytes
+byte b2[1];
+float DutyCycleRecieved; //create variable to store duty cycle command calculated from RPI
+float finished = 50.0001;
+int count; //variable used to create logic same as ROS side to prevent excessively bad packets
 
 void loop() {
   axis1 = Hall_One.read() + 2147483648;
@@ -27,11 +32,21 @@ void loop() {
   //Serial.write((uint8_t*)&axis1, sizeof(axis1)); //split up the axis1 variable into byte-sized pieces and send over serial
   //Serial.write((uint8_t*)&axis2, sizeof(axis2));
   if (Serial.available() >= 6) { //if there are 6 or bytes available to read
-    float DutyCycleRecieved;
-    byte buffer[2];
-    Serial.readBytes((char*)buffer,2);
-    Serial.readBytes((char*)&DutyCycleRecieved, 6); //read 6 bytes into the buffer
-    Serial.flush(); //flush transmission
+    //I think this all needs to be one function
+    Serial.readBytes((char*)b1, 1);
+    Serial.readBytes((char*)b2, 1); //read in first two bytes of the transmission to see if they match start sequence
+    while ((b1[0] != 0xAA) and (b2[0] != 0xAA)){ //while the first two bytes are not the 170 start bytes
+      b1[0] = b2[0]; //this logic should create the improved step through for better performance that i was looking for
+      Serial.readBytes((char*)b2, 1); 
+      count++;
+      if (count >= 5){
+        while (Serial.available() > 0){
+          Serial.read(); //read off any incoming data to nothing to clear it
+        }
+        count = 0; //if we get continuously bad packets this will go on forever... putting this all in a function with a return will solve this but not in the mood
+      }
+    }
+    Serial.readBytes((char*)&DutyCycleRecieved, 4); //next four bytes should be the good floating point duty cycle
     float DutyDouble = DutyCycleRecieved * 2.0;
     Serial.write((uint8_t*)&DutyDouble, sizeof(DutyDouble));
   }
